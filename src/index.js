f5b49f414cc1333b5aca4047c3aaf3872351c169
Ivan Shanin <ivanxshanin@gmail.com>
2023-08-10 15:53:18 +0300

    populate photo gallery

--- src/index.js
+++ src/index.js
@@ -14,3 +14,29 @@ const headerSettings = {
 
 const header = new Header(headerSettings);
 header.setEventListeners();
+
+
+
+// scrollable photo gallery
+
+import Gallery from './components/Gallery';
+
+import rearViewImage from '../images/rear_view.png';
+import driverSeatImage from '../images/driver_seat.png';
+import passengerSeatImage from '../images/passenger_seat.png';
+
+// might as well collect photos from an API endpoint
+const pictureSourcesList = [
+    rearViewImage,
+    driverSeatImage,
+    passengerSeatImage
+];
+
+const galleryOptions = {
+    pictureSelector: ".gallery__picture", 
+    switcherButtonClass: ".gallery__switcher-button",
+};
+
+const gallery = new Gallery(pictureSourcesList, galleryOptions);
+gallery.setInitialSourceIndex(0);
+gallery.setEventListeners();
