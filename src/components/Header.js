d89c05d675d4034b2704693da9b74c49e98f81ec
Ivan Shanin <ivanxshanin@gmail.com>
2023-08-10 15:51:57 +0300

    make header navigation interactive

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+export default class Header {
+    constructor(
+        {
+            headerSwitcherClass, 
+            headerLogoAreaSelector, 
+            headerNavigationSelector
+        }
+    ) {
+        this._headerSwitcherClass = headerSwitcherClass;
+        this._headerSwitcher = document.querySelector(`.${headerSwitcherClass}`);
+        this._headerLogoArea = document.querySelector(headerLogoAreaSelector);
+        this._headerNavigation = document.querySelector(headerNavigationSelector);
+    };
+
+    _revealNavigation() {
+        this._headerSwitcher.classList.remove(`${this._headerSwitcherClass}_reveal`);
+        this._headerSwitcher.classList.add(`${this._headerSwitcherClass}_hide`);
+        this._headerLogoArea.style.display = "none";
+        this._headerNavigation.style.display = "flex";     
+    };
+
+    _hideNavigation() {
+        this._headerSwitcher.classList.remove(`${this._headerSwitcherClass}_hide`);
+        this._headerSwitcher.classList.add(`${this._headerSwitcherClass}_reveal`);
+        this._headerLogoArea.style.display = "flex";
+        this._headerNavigation.style.display = "none";  
+    };
+
+    _toggleMenu() {
+        if (
+            this._headerSwitcher.classList.contains(
+                `${this._headerSwitcherClass}_reveal`
+            )
+        ) {
+            this._revealNavigation();
+        } else {
+            this._hideNavigation();
+        };
+    };
+
+    setEventListeners() {
+        this._headerSwitcher.addEventListener(
+            "click", this._toggleMenu.bind(this)
+        );
+    };
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './index.scss';
 
-// for testing purposes
 
-const numbers = [2, 3, 5];
-const doubledNumbers = numbers.map(number => number * 2);
-console.log(doubledNumbers); // 4, 6, 10
+
+// interactive header navigation
+
+import Header from './components/Header';
+
+const headerSettings = {
+    headerSwitcherClass: "header__switcher-button",
+    headerLogoAreaSelector: ".header__logo-area",
+    headerNavigationSelector: ".header__nav"
+}
+
+const header = new Header(headerSettings);
+header.setEventListeners();
