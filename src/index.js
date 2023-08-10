43a61fcc7ade65b371f1267b6e75f50b5dcbcc2f
Ivan Shanin <ivanxshanin@gmail.com>
2023-08-10 15:54:22 +0300

    populate external articles section

--- src/index.js
+++ src/index.js
@@ -40,3 +40,39 @@ const galleryOptions = {
 const gallery = new Gallery(pictureSourcesList, galleryOptions);
 gallery.setInitialSourceIndex(0);
 gallery.setEventListeners();
+
+
+
+// external articles section
+
+import Article from './components/Article';
+
+// might as well collect articles from an API endpoint
+const articlesObjectList = [
+    {
+        magazine: "Drive.ru",
+        title: "Вэн Volkswagen e-Bulli скрестил классику с современной техникой",
+        link: "https://www.drive.ru/news/volkswagen/5e7447bdec05c4b251000010.html"
+    },
+    {
+        magazine: "Engadget",
+        title: "VW’s e-BULLI concept shows how your classic van can become an EV",
+        link: "https://www.engadget.com/2020-03-20-vw-unveils-e-bulli-t1-electric-conversion.html"
+    }
+];
+
+const articleSettings = {
+    linkSelector: ".press__link",
+    titleSelector: ".press__article-title",
+    magazineSelector: ".press__article-magazine",
+    switcherButtonsContainerSelector: ".press__switcher-area",
+    switcherButtonTemplateSelector: "#press__switcher-button",
+    switcherButtonElementClass: "press__switcher-button"
+};
+
+articlesObjectList.forEach(
+    articleObject => {
+        const article = new Article(articleObject, articleSettings);
+        article.generate();
+    }
+);
